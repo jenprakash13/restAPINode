324eb188fe4655aa5d73e64803fc040921dd75f9
jenprakash13 <prakash17mca@gmail.com>
2020-01-29 00:11:12 +0530

    cors add in the index.js file and database connection properties change(cloud DB add)

--- index.js
+++ index.js
@@ -1,12 +1,28 @@
 const express = require("express");
 const mysql = require("mysql");
 const bodyParser = require("body-parser");
+var cors = require('cors');
 
 
 const user = require("./model/user");
 
 const app = express();
+app.use(cors())
 app.listen(7000);
+
+// app.use((req , res , next) =>{
+
+//     res.header("Access-Control-Allow-Origin","*");
+//     res.header("Access-Control-Allow-Headers",
+//     "Origin, X-Requested-With, Content-Type, Accept, Authorization"
+//     );
+
+//     // if(req.method === 'OPTIONS'){
+//     //     res.header('Access-Control-Allow-Methods','GET,POST,PUT,PATCH,DELETE');
+//     //     return res.status(200).json({});
+//     // }
+
+// });
 
 
 //app.use(bodyParser.urlencoded({extended:false}));
@@ -39,7 +55,7 @@ app.post("/user", function(req,res){
             if(err){
                 throw err;
             }else{
-
+                //res.send(data);
                 user.getUser(data.insertId,function(err,data){
             if(err){
                 throw err;
--- model/user.js
+++ model/user.js
@@ -1,10 +1,10 @@
 const mysql = require("mysql");
 
 const db = mysql.createConnection({
-    host : "localhost",
-    user : "root",
-    password : "root",
-    database : "springdata"
+    host : "remotemysql.com",
+    user : "bJUuLgvO4O",
+    password : "LlWxfxxdXi",
+    database : "bJUuLgvO4O"
 });
 
 db.connect(function(err){
@@ -17,7 +17,7 @@ db.connect(function(err){
 });
 
 exports.getUser = function(id,callback){
-    let sql = 'SELECT  * from person where id = ?';
+    let sql = 'SELECT  * from Data where id = ?';
     console.log(id);
     db.query(sql,[id], function(err,data){
 
@@ -32,7 +32,7 @@ exports.getUser = function(id,callback){
 
 exports.insertUser = function(data,callback){
 
-    let sql =" insert into person set ?";
+    let sql =" insert into Data set ?";
 
     db.query(sql,[data], function(err,data){
         if(err){
@@ -45,7 +45,7 @@ exports.insertUser = function(data,callback){
 
 exports.updateUser = function(id,data,callback){
 
-    let sql = "update person set ? where id = ?";
+    let sql = "update Data set ? where id = ?";
 
     db.query(sql,[data,id],function(err,data){
         if(err){
@@ -59,7 +59,7 @@ exports.updateUser = function(id,data,callback){
 
 exports.deleteUser = function(id,callback){
 
-    let sql = "delete from person where id = ?";
+    let sql = "delete from Data where id = ?";
 
     db.query(sql,[id], function(err,data){
         if(err){
